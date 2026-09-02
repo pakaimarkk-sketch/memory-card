cb5f392d394d822b0d55d826b8135a5f086468aa
pakaimarkk <251377257+pakaimarkk-sketch@users.noreply.github.com>
2026-09-02 19:02:53 +0200

    feat: add UI for Cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,24 @@
-import { cards } from "./game/cards";
-import { Card } from "./components/Card";
-import { ResultScreen } from "./components/ResultScreen";
-import { useMemoryGame } from "./hooks/useMemoryGame";
+import { useState } from "react";
+import { StarterScreen } from "./components/StarterScreen";
+import { MemoryGame } from "./components/MemoryGame";
+import { startGame } from "./game/gameSelection";
 
-export function App() {}
+export function App() {
+  const [deck, setDeck] = useState([]);
+  const [screen, setScreen] = useState("setup");
+
+  async function handleStartGame(setup) {
+    const newDeck = await startGame(setup);
+
+    setDeck(newDeck);
+    setScreen("game");
+  }
+
+  if (screen === "setup") {
+    return <StarterScreen onStartGame={handleStartGame} />;
+  }
+
+  if (screen === "game") {
+    return <MemoryGame deck={deck} />;
+  }
+}
