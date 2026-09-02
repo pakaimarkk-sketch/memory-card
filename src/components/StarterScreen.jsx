b56bd064bf780969933d43a5434e3db7e5a80cac
pakaimarkk <251377257+pakaimarkk-sketch@users.noreply.github.com>
2026-09-02 22:04:24 +0200

    feat: add game description

--- src/components/StarterScreen.jsx
+++ src/components/StarterScreen.jsx
@@ -26,6 +26,13 @@ export function StarterScreen({ onStartGame }) {
   return (
     <section className="starter-screen">
       <h1 className="difficulty-header">Select {setupStep}</h1>
+      <p className="game-description">
+        Test your memory by clicking each Pokémon only once. The cards shuffle
+        after every selection, so keep track of which Pokémon you've already
+        chosen. <br />
+        <strong> Click the same Pokémon twice and the game is over. </strong>
+        Choose a difficulty and try to reach the highest score!
+      </p>
       <div className="difficulty-options">
         {gameSelections[setupStep].map((option) => (
           <button
